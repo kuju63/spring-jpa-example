3f2827829ef36376ed0dd82621b840ca6909c478
Jun Kurihara <lh182051+src@gmail.com>
2025-08-14 17:14:28 +0900

    refactor UserController to include group filtering in user retrieval

--- src/main/java/io/github/kuju63/example/demo2/UserController.java
+++ src/main/java/io/github/kuju63/example/demo2/UserController.java
@@ -1,6 +1,5 @@
 package io.github.kuju63.example.demo2;
 
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +16,15 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getUsers(@RequestParam(required = false) String username, @RequestParam(required = false) String email) {
-        return userRepository.findAll(Specification.allOf(UserSpecification.byEnabled(true).and(UserSpecification.byName(username).or(UserSpecification.byEmail(email)))));
+        return userRepository.findAll(
+                UserSpecification.byEnabled(true)
+                        .and(UserSpecification.byEnabledGroup(true))
+                        .and(UserSpecification.byEmail(email).or(UserSpecification.byName(username))));
     }
 
     @GetMapping("/users/{userId}")
     public User getUser(@PathVariable UUID userId) throws UserNotFoundException {
+        // Need to filter by enabled user and enabled group
         return userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
     }
